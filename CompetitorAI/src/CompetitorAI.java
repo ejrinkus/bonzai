4daf6a8ddd3d66069be41494fe6d7e4eb2cdfbaa
Joshua Manela <jmmanela@mtu.edu>
2014-04-05 15:47:35 -0400

    hashtag omg

--- CompetitorAI/src/CompetitorAI.java
+++ CompetitorAI/src/CompetitorAI.java
@@ -3,54 +3,54 @@ import bonzai.api.*;
 import java.util.*;
 
 public class CompetitorAI implements AI {
-	
+
 	private WeightComparator pathWeight = new CompetitorWeightComparator();
 	private boolean firstTurn = true;
 
 	public static int myTeam = -1;
 	public static int scores[] = new int[4];
 
-    private ArrayList<Node> wizardPath;
-    private ArrayList<Node> explorePoints;
-    private int wizardExploreIndex;
-	
+	private ArrayList<Node> wizardPath;
+	private ArrayList<Node> explorePoints;
+	private int wizardExploreIndex;
+
 	private Node mapGuess[][];
 	private Node bases[];
 
-	
+
 	/**
 	 * You must have this function, all of the other functions in 
 	 * this class are optional.
 	 */
 	@Override
 	public void takeTurn(AIGameState state) {
-		
-		
+
+
 		if(firstTurn) {
-            explorePoints = new ArrayList<Node>();
-            int size = state.getNumberOfPlayers();
-            for(int i = 1; i <= size; i++){
-                explorePoints.add(state.getBase(i));
-            }
-            explorePoints.add(state.getNode(state.getWidth() / 2, state.getHeight() / 2));
-            explorePoints.add(state.getNode(state.getWidth() / 2, 0));
-            explorePoints.add(state.getNode(0, state.getHeight() / 2));
-            explorePoints.add(state.getNode(state.getWidth() / 2, state.getHeight() - 1));
-            explorePoints.add(state.getNode(state.getWidth() - 1, state.getHeight() / 2));
-            wizardExploreIndex = 0;
+			explorePoints = new ArrayList<Node>();
+			int size = state.getNumberOfPlayers();
+			for(int i = 1; i <= size; i++){
+				explorePoints.add(state.getBase(i));
+			}
+			explorePoints.add(state.getNode(state.getWidth() / 2, state.getHeight() / 2));
+			explorePoints.add(state.getNode(state.getWidth() / 2, 0));
+			explorePoints.add(state.getNode(0, state.getHeight() / 2));
+			explorePoints.add(state.getNode(state.getWidth() / 2, state.getHeight() - 1));
+			explorePoints.add(state.getNode(state.getWidth() - 1, state.getHeight() / 2));
+			wizardExploreIndex = 0;
 			takeFirstTurn(state);
 			firstTurn = false;
 		}
-		
+
 		updateGuess(state);
-		
+
 		this.wizardBrain(state);
 		this.moveBlockers(state);
 		this.moveCleaners(state);
 		this.moveScouts(state);
 		this.moveHats(state);
 	}
-	
+
 	/**
 	 * Executes only on your first turn.
 	 * @param state
@@ -59,15 +59,15 @@ public class CompetitorAI implements AI {
 		for(Actor a : state.getMyActors()) {
 			a.shout("THIS. IS. HOGWAARRTTSS!!!!"); //Shout "Go Team!" for the first turn
 		}
-		
+
 		myTeam = state.getMyTeamNumber();
-		
+
 		//setup the map-saving
 		mapGuess = new Node[state.getWidth()][state.getHeight()];
 		bases = new Node[state.getNumberOfPlayers()];
 		//save all spaces we can see at the start
 	}
-	
+
 	/**
 	 * Move or castMagic with your Wizard
 	 * @param state
@@ -75,104 +75,115 @@ public class CompetitorAI implements AI {
 	private void wizardBrain(AIGameState state) {
 		Wizard wizard = state.getMyWizard();
 
-        //Can't see anything, so explore
-        if(state.getNeutralHats().size() == 0
-                && state.getEnemyHats().size() == 0){
-            if(wizardPath == null){
-                if(wizard.getLocation().getX() == explorePoints.get(wizardExploreIndex).getX() &&
-                   wizard.getLocation().getY() == explorePoints.get(wizardExploreIndex).getY()){
-                    wizardExploreIndex++;
-                }
-                if(wizardExploreIndex >= explorePoints.size()) wizardExploreIndex = 0;
-                wizardPath = state.getPath(wizard, explorePoints.get(wizardExploreIndex), pathWeight);
-            }
-            if(wizard.canMove(wizard.getDirection(wizardPath)))
-                wizard.move(wizard.getDirection(wizardPath));
-            else if(!wizardPath.get(0).isNaturalWall()){
-                for(Actor a : wizardPath.get(0).getActors()){
-                    if(a instanceof Blocker && a.getTeam() != myTeam){
-                        wizard.castMagic(a);
-                    }
-                    if(a instanceof Blocker && a.getTeam() == myTeam){
-                        ((Blocker) a).unBlock();
-                    }
-                }
-            }
-            else
-                wizardPath = null;
-        }
+		//Can't see anything, so explore
+		if(state.getNeutralHats().size() == 0
+				&& state.getEnemyHats().size() == 0){
+			if(wizardPath == null){
+				if(wizard.getLocation().getX() == explorePoints.get(wizardExploreIndex).getX() &&
+						wizard.getLocation().getY() == explorePoints.get(wizardExploreIndex).getY()){
+					wizardExploreIndex++;
+				}
+				if(wizardExploreIndex >= explorePoints.size()) wizardExploreIndex = 0;
+				wizardPath = state.getPath(wizard, explorePoints.get(wizardExploreIndex), pathWeight);
+			}
+			if(wizard.canMove(wizard.getDirection(wizardPath)))
+				wizard.move(wizard.getDirection(wizardPath));
+			else if(!wizardPath.get(0).isNaturalWall()){
+				for(Actor a : wizardPath.get(0).getActors()){
+					if(a instanceof Blocker && a.getTeam() != myTeam){
+						wizard.castMagic(a);
+					}
+				}
+			}
+			else
+				wizardPath = null;
+		}
 
-        //Strat 1 - low pts, low mana
-        {
-            if(state.getNeutralHats().size() > 0){
-                Actor closest = this.findClosest(wizard, state.getNeutralHats());
-                double dist = this.dist(wizard, closest);
-                if(dist == 1.0){
-                    if(wizard.canCast(closest))
-                        wizard.castMagic(closest);
-                }
-                else{
-                    wizardPath = state.getPath(wizard,
-                            closest.getLocation(),
-                            pathWeight);
-                }
-            }
-        }
 
-        //Strat 2 - high pts, low mana
-        {
+		Actor a = null;
+		Node b = a.getLocation();
 
-        }
+		for( int i=0; i<4; i++ ){
+			for( Actor gg : b.getNearby(i).getActors() ){
 
-        //Strat 3 - low pts, high mana
-        {
+			}
 
-        }
+		}
 
-        //Strat 4 - high pts, high mana
-        {
+		//Strat 1 - low pts, low mana
+		{
+			if(state.getNeutralHats().size() > 0){
+				Actor closest = this.findClosest(wizard, state.getNeutralHats());
+				double dist = this.dist(wizard, closest);
+				if(dist == 1.0){
+					if(wizard.canCast(closest))
+						wizard.castMagic(closest);
+				}
+				else{
+					wizardPath = state.getPath(wizard,
+							closest.getLocation(),
+							pathWeight);
+				}
+			}
+		}
 
-        }
+		//Strat 2 - high pts, low mana
+		{
+
+		}
+
+		//Strat 3 - low pts, high mana
+		{
+
+		}
+
+		//Strat 4 - high pts, high mana
+		{
+
+		}
 	}
-	
+
 	/**
 	 * Move, block, or unBlock with your blockers.
 	 * @param state
 	 */
 	private void moveBlockers(AIGameState state) {
-		
+
 		ArrayList<Wizard> enemyWiz = state.getEnemyWizards();
 		final int NEAREST_DIST = 3;
-		
-		
+
+
 		for(Blocker blocker : state.getMyBlockers()) {
-			
+
 			if( dist(blocker, state.getMyWizard()) < NEAREST_DIST){
 				blocker.unBlock();
 				continue;
 			}
-			
-			Wizard precious = (Wizard) findClosest(blocker, enemyWiz);
-			
-			int wizdist = (int) Math.floor( dist( blocker, precious ) );
-			if( wizdist < NEAREST_DIST ) {
-				blocker.block();
+
+			if( enemyWiz != null ){
+				Wizard precious = (Wizard) findClosest(blocker, enemyWiz);
+				int wizdist = (int) Math.floor( dist( blocker, precious ) );
+				if( wizdist < NEAREST_DIST ){
+					blocker.block();
+				}
 			} else {
+				blocker.unBlock();
 				state.getPath(blocker, state.getMyBase(), pathWeight);
 			}
+
 		}
 	}
-	
+
 	/**
 	 * Move or sweep with your cleaners.
 	 * @param state
 	 */
 	private void moveCleaners(AIGameState state) {
-		
-		
+
+
 		for(Cleaner cleaner : state.getMyCleaners()) {
 			int moveDirection = cleaner.getDirection(state.getNode(2, 2), pathWeight);
-			
+
 			//Move your cleaner one step closer to the node (1, 1)
 			if(!cleaner.move(moveDirection)) {
 				cleaner.shout("I am unable to move in that direction!");
@@ -181,38 +192,39 @@ public class CompetitorAI implements AI {
 					//There is a blocking blocker in the direction of 'moveDirection'
 				}
 			}
-			
+
 			//If the sweeper can, it uses it's ability on a blocker instead of moving.
 			for(Blocker enemyBlocker : state.getEnemyBlockers()) {
 				if(cleaner.isAdjacent(enemyBlocker)) {
 					cleaner.sweep(enemyBlocker);
 				}
 			}
-			
+
 		}
 	}
-	
+
 	/**
 	 * Move with your scouts.
 	 * @param state
 	 */
 	private void moveScouts(AIGameState state) {
-		
+
 		int numScouts = state.getMyScouts().size();
 		boolean firstScout = true;
 		Node set;
-		
+
 		for(Scout scout : state.getMyScouts()) {
-			
+
 			if( firstScout ){
-				
+
 				//follow wizard so you can see people
-				scout.doubleMove(state.getPath(scout, state.getMyWizard().getLocation(), pathWeight));
-				
+				scout.doubleMove(state.getPath(scout, state.getMyWizard().getLocation(), pathWeight));				
 				firstScout = false;
-				
+
+
+
 			}
-			
+
 
 			//move randomly and see what's up
 			scout.doubleMove((int)(Math.random()*4), (int)(Math.random()*4));
@@ -220,7 +232,7 @@ public class CompetitorAI implements AI {
 
 		}
 	}
-	
+
 	private int getStrat(AIGameState state){
 		int count = 0;
 		int highscore = 0;
@@ -229,7 +241,7 @@ public class CompetitorAI implements AI {
 				highscore = state.getPlayerScore(i);
 			}
 		}
-		
+
 		if(state.getPlayerScore(myTeam) >= highscore && state.getMyMana() > 400){
 			return 4;//we are the best so let's screw with people
 		}
@@ -243,7 +255,7 @@ public class CompetitorAI implements AI {
 			return 1; //low pts  low mana
 		}
 	}
-	
+
 	/**
 	 * Do something with your hats!!!
 	 * @param state
@@ -269,40 +281,40 @@ public class CompetitorAI implements AI {
 			}
 		}
 	}
-	
+
 	private void updateGuess(AIGameState state) {
-		
+
 		Node cur;
-		
+
 		for( int i=0; i<state.getWidth(); i++ )
 			for(int j=0; j<state.getHeight(); j++ ){
 				cur = state.getNode(i, j);
-				
+
 				if( cur == null || !cur.isVisible() ) continue;
-				
+
 				mapGuess[cur.getX()][cur.getY()] = cur;
 			}
-		
+
 	}
 
-    private Actor findClosest(Actor start, ArrayList<? extends Actor> list){
-        double minDist = -1;
-        double temp = 0;
-        Actor closest = null;
-        for(Actor a : list){
-            temp = dist(start, a);
-            if(closest == null || temp < minDist){
-                closest = a;
-                minDist = temp;
-            }
-        }
-        return closest;
-    }
+	private Actor findClosest(Actor start, ArrayList<? extends Actor> list){
+		double minDist = -1;
+		double temp = 0;
+		Actor closest = null;
+		for(Actor a : list){
+			temp = dist(start, a);
+			if(closest == null || temp < minDist){
+				closest = a;
+				minDist = temp;
+			}
+		}
+		return closest;
+	}
 
-    private double dist(Actor a, Actor b){
-        int x = Math.abs(a.getLocation().getX() - b.getLocation().getX());
-        int y = Math.abs(a.getLocation().getY() - b.getLocation().getY());
-        return Math.sqrt((x * x) + (y * y));
-    }
+	private double dist(Actor a, Actor b){
+		int x = Math.abs(a.getLocation().getX() - b.getLocation().getX());
+		int y = Math.abs(a.getLocation().getY() - b.getLocation().getY());
+		return Math.sqrt((x * x) + (y * y));
+	}
 
 }
